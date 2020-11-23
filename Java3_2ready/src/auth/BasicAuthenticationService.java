4bace6180d7cc752a424931eb2fd9503dba795ba
Artem <rav051098@gmail.com>
2020-11-23 17:30:37 +0300

    server homework

---
+++ Java3_2ready/src/auth/BasicAuthenticationService.java
@@ -0,0 +1,25 @@
+package auth;
+
+import database.DBService;
+import entity.User;
+
+import java.util.Optional;
+
+public class BasicAuthenticationService implements AuthenticationService {
+    /**
+     * Fake database with stubbed entities
+     */
+
+
+    @Override
+    public Optional<User> doAuth(String email, String password) {
+        DBService dbService = new DBService();
+
+        User newUser = dbService.findUser(email);
+
+        if (newUser.getPassword().equals(password)) {
+            return Optional.of(newUser);
+        }
+        return Optional.empty();
+    }
+}
