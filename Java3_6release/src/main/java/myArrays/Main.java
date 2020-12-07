0f5b325628f9ddbe49ee9c5b7cedf8d96dedaf23
Artem <rav051098@gmail.com>
2020-12-07 10:00:36 +0300

    homework junit tests

---
+++ Java3_6release/src/main/java/myArrays/Main.java
@@ -0,0 +1,22 @@
+package myArrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * Для отладки MyArrays методов
+         */
+
+        MyArrays myArrays = new MyArrays();
+
+        int[] array = {
+                2, 4, 8, 9, 0
+        };
+
+        int[] array2 = {};
+
+       // myArrays.findNumbersAfterLastFourAtArray(array);
+
+        System.out.println(myArrays.checkArrayForOneAndFour(array2));
+    }
+}
